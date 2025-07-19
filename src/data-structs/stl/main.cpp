3a14e9541f7ee7d906b892d610745c196cdc40f1
Yeo Jing Kang <yeojingkang@gmail.com>
2025-07-19 22:02:35 +0800

    [STL] Implement more vector functions

--- src/data-structs/stl/main.cpp
+++ src/data-structs/stl/main.cpp
@@ -1,8 +1,66 @@
 #include <vector>
 #include "vector.h"
 
+#include <iostream>
+
+class C
+{
+public:
+    C() : x(0) { std::cout << "C default ctor" << std::endl; }
+    C(const C& c) : x(c.x) {  std::cout << "C copy ctor" << std::endl; }
+    C(C&& c) : x(std::move(c.x)) {  std::cout << "C move ctor" << std::endl; }
+    ~C() { std::cout << "C dtor" << std::endl; }
+
+    explicit C(int i) : x(i) { std::cout << "value ctor: " << i << std::endl; }
+
+    C& operator=(const C& c)
+    {
+        if (&c == this)
+            return *this;
+
+        x = c.x;
+        std::cout << "C copy assignment" << std::endl;
+        return *this;
+    }
+
+    C& operator=(C&& c)
+    {
+        x = std::move(c.x);
+        std::cout << "C move assignment" << std::endl;
+        return *this;
+    }
+
+    int x;
+};
+
+template<template<typename> typename T>
+void testC(const std::string& name)
+{
+    std::cout << name << ":" << std::endl;
+
+    T<C> cv(2);
+    T<C> cv2(3);
+    auto mcv = std::move(cv);
+    auto ccv2 = cv2;
+
+    std::cout << "push_back:" << std::endl;
+    T<C> pv;
+    for (auto i = 0; i < 5; ++i)
+        pv.push_back(C{i});
+    for (auto i = 0; i < 5; ++i)
+        std::cout << pv[i].x << "  ";
+    std::cout << std::endl;
+}
+
 int main()
 {
     mystl::vector<int> a(10);
     mystl::vector<int> b;
+
+
+    testC<mystl::vector>("mystl vector");
+
+    std::cout << std::endl;
+
+    testC<std::vector>("std vector");
 }
--- src/data-structs/stl/vector.h
+++ src/data-structs/stl/vector.h
@@ -23,34 +23,147 @@ namespace mystl
         using const_reference = const value_type&;
         using pointer = alloc_traits::pointer;
         using const_pointer = alloc_traits::const_pointer;
-        // TODO: Need to define own iterator class?
-        //using iterator = int;
-        //using const_iterator = int;
-        //using reverse_iterator = std::reverse_iterator<iterator>;
-        //using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+        using iterator = pointer;
+        using const_iterator = const_pointer;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+        // Constructors
+        /**********************************************************************/
         vector() : vector(Allocator()) {}
         explicit vector(const Allocator& alloc) :
-            _alloc{alloc}, _data{nullptr}, _sz{0}, _cap{0}
+            _alloc{alloc}, _sz{0}, _cap{0}, _data{nullptr}
         {}
 
         explicit vector(size_type count, const Allocator& alloc = Allocator()) :
-            _alloc{alloc}, _data{alloc_traits::allocate(_alloc, count)}, _sz{count}, _cap{count}
+            _alloc{alloc}, _sz{count}, _cap{count}, _data{alloc_traits::allocate(_alloc, count)}
         {
             for (size_type i = 0; i < _sz; ++i)
                 alloc_traits::construct(_alloc, _data + i);
         }
 
+        vector(size_type count, const T& value, const Allocator& alloc = Allocator()) :
+            _alloc{alloc}, _sz{count}, _cap{count}, _data{alloc_traits::allocate(_alloc, count)}
+        {
+            for (size_type i = 0; i < _sz; ++i)
+                alloc_traits::construct(_alloc, _data + i, value);
+        }
+
+        template<typename InputIt>
+        vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) :
+            _alloc{alloc}, _sz{std::distance(first, last)}, _cap{_sz}, _data{alloc_traits::allocate(_alloc, _cap)}
+        {
+            std::uninitialized_copy(first, last, _data);
+        }
+
+        vector(const vector& other) :
+            _alloc{alloc_traits::select_on_container_copy_construction(other.get_allocator())},
+            _sz{other._sz},
+            _cap{other._cap},
+            _data{alloc_traits::allocate(_alloc, _cap)}
+        {
+            std::uninitialized_copy_n(other._data, other._sz, _data);
+        }
+
+        vector(vector&& other) :
+            _alloc{std::move(other._alloc)},
+            _sz{std::move(other._sz)},
+            _cap{std::move(other._cap)},
+            _data{std::move(other._data)}
+        {
+            other._sz = 0;
+            other._data = nullptr;
+        }
+
+        vector(std::initializer_list<T> init, const Allocator& alloc = Allocator()) :
+            vector(init.begin(), init.end(), alloc)
+        {}
+        /**********************************************************************/
+
         ~vector()
         {
+            if (_data == nullptr)
+                return;
+
             for (size_type i = 0; i < _sz; ++i)
                 alloc_traits::destroy(_alloc, _data + i);
             alloc_traits::deallocate(_alloc, _data, _cap);
         }
 
+        // Assignments
+        /**********************************************************************/
+        vector& operator=(const vector& other)
+        {
+            if (this == &other)
+                return *this;
+
+            // TODO: Implement
+
+            return * this;
+        }
+
+        vector& operator=(vector&& other)
+        {
+            if (this == &other)
+                return *this;
+
+            _alloc = std::move(other._alloc);
+            _sz = std::move(other._sz);
+            _cap = std::move(other._cap);
+            _data = std::move(other._data);
+            return *this;
+        }
+        /**********************************************************************/
+
+        reference operator[](size_type pos) { return _data[pos]; }
+        const_reference operator[](size_type pos) const { return _data[pos]; }
+
+        void push_back(const T& value)
+        {
+            maybe_expand();
+            alloc_traits::construct(_alloc, _data + _sz++, value);
+        }
+        void push_back(T&& value)
+        {
+            maybe_expand();
+            alloc_traits::construct(_alloc, _data + _sz++, std::move(value));
+        }
+
+        bool empty() const { return _sz == 0; }
+        size_type size() const { return _sz; }
+        allocator_type get_allocator() const { return _alloc; }
+
+        void swap(vector& other)
+        {
+            if constexpr (alloc_traits::propagate_on_container_swap::value)
+                std::swap(_alloc, other._alloc);
+
+            std::swap(_sz, other._sz);
+            std::swap(_cap, other._cap);
+            std::swap(_data, other._data);
+        }
+
     private:
         allocator_type _alloc;
-        pointer _data;
         size_type _sz, _cap;
+        pointer _data;
+
+        void maybe_expand()
+        {
+            if (_sz < _cap)
+                return;
+
+            auto newCap = _cap == 0 ? 1 : _cap;
+            while (newCap < _sz) newCap <<= 1; // Keep doubling until large enough
+            auto newData = alloc_traits::allocate(_alloc, newCap);
+            std::uninitialized_move_n(_data, _sz, newData);
+
+            std::swap(_cap, newCap);
+            std::swap(_data, newData);
+            // Shouldn't need to destroy old array elements
+            for (size_type i = 0; i < _sz; ++i)
+                alloc_traits::destroy(_alloc, newData + i);
+            alloc_traits::deallocate(_alloc, newData, newCap);
+        }
     };
 }
