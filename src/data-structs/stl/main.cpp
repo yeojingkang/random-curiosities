380acc6b1a943edc0b1e91ea6509f5925b41ec38
Yeo Jing Kang <yeojingkang@gmail.com>
2025-07-22 22:58:46 +0800

    [STL] Implement vector element access & iterators

--- src/data-structs/stl/main.cpp
+++ src/data-structs/stl/main.cpp
@@ -33,22 +33,95 @@ public:
     int x;
 };
 
+std::ostream& operator<<(std::ostream& os, const C& c)
+{
+    os << c.x;
+    return os;
+}
+
+template<typename T>
+void testCtor()
+{
+    std::cout << "ctors:" << std::endl;
+    T v(2);
+    T v2(3);
+    auto mcv = std::move(v);
+    auto cv2 = v2;
+}
+
+template<typename T>
+void testPushBack()
+{
+    std::cout << "push_back:" << std::endl;
+    T pv;
+    for (auto i = 0; i < 5; ++i)
+    {
+        pv.push_back(C{i});
+    }
+    for (auto i = 0; i < 5; ++i)
+        std::cout << pv[i] << "  ";
+    std::cout << std::endl;
+}
+
+template<typename T>
+void testElementAccess()
+{
+    std::cout << "element access:" << std::endl;
+    const T cv(3);
+    T v{C{1},C{2},C{3}};
+    std::cout << "cv: " << cv.at(0) << "  " << cv[1] << std::endl;
+    std::cout << "pv: " << v.at(0) << "  " << v[1] << std::endl;
+    //std::cout << "out_of_range: " << v.at(5) << std::endl; // out of range
+
+    std::cout << "front: " << cv.front() << ", " << v.front() << std::endl;
+    std::cout << "back: " << cv.back() << ", " << v.back() << std::endl;
+    std::cout << "data: " << (&cv[0] == cv.data()) << " " << (&v[0] == v.data()) << std::endl;
+}
+
+template<typename T>
+void testIterators()
+{
+    std::cout << "iterators:" << std::endl;
+
+    const T cv(5);
+    T v{C{1},C{2},C{3}, C{4}, C{5}};
+
+    std::cout << "cv normal: ";
+    for (auto it = std::cbegin(cv); it != std::cend(cv); ++it)
+        std::cout << *it << "  ";
+    std::cout << std::endl;
+
+    std::cout << "v normal: ";
+    for (auto it = std::begin(v); it != std::end(v); ++it)
+        std::cout << *it << "  ";
+    std::cout << std::endl;
+
+    std::cout << "cv rev: ";
+    for (auto it = std::crbegin(cv); it != std::crend(cv); ++it)
+        std::cout << *it << "  ";
+    std::cout << std::endl;
+
+    std::cout << "v rev: ";
+    for (auto it = std::rbegin(v); it != std::rend(v); ++it)
+        std::cout << *it << "  ";
+    std::cout << std::endl;
+}
+
 template<template<typename> typename T>
 void testC(const std::string& name)
 {
     std::cout << name << ":" << std::endl;
 
-    T<C> cv(2);
-    T<C> cv2(3);
-    auto mcv = std::move(cv);
-    auto ccv2 = cv2;
+    testCtor<T<C>>();
+    std::cout << std::endl;
 
-    std::cout << "push_back:" << std::endl;
-    T<C> pv;
-    for (auto i = 0; i < 5; ++i)
-        pv.push_back(C{i});
-    for (auto i = 0; i < 5; ++i)
-        std::cout << pv[i].x << "  ";
+    testPushBack<T<C>>();
+    std::cout << std::endl;
+
+    testElementAccess<T<C>>();
+    std::cout << std::endl;
+
+    testIterators<T<C>>();
     std::cout << std::endl;
 }
 
--- src/data-structs/stl/vector.h
+++ src/data-structs/stl/vector.h
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <memory>
 #include <iterator>
+#include <format>
 
 namespace mystl
 {
@@ -30,8 +31,9 @@ namespace mystl
 
         // Constructors
         /**********************************************************************/
-        vector() : vector(Allocator()) {}
-        explicit vector(const Allocator& alloc) :
+        constexpr vector() noexcept(noexcept(Allocator())) : vector(Allocator()) {}
+
+        constexpr explicit vector(const Allocator& alloc) :
             _alloc{alloc}, _sz{0}, _cap{0}, _data{nullptr}
         {}
 
@@ -42,7 +44,7 @@ namespace mystl
                 alloc_traits::construct(_alloc, _data + i);
         }
 
-        vector(size_type count, const T& value, const Allocator& alloc = Allocator()) :
+        constexpr vector(size_type count, const T& value, const Allocator& alloc = Allocator()) :
             _alloc{alloc}, _sz{count}, _cap{count}, _data{alloc_traits::allocate(_alloc, count)}
         {
             for (size_type i = 0; i < _sz; ++i)
@@ -50,13 +52,13 @@ namespace mystl
         }
 
         template<typename InputIt>
-        vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) :
-            _alloc{alloc}, _sz{std::distance(first, last)}, _cap{_sz}, _data{alloc_traits::allocate(_alloc, _cap)}
+        constexpr vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) :
+            _alloc{alloc}, _sz{static_cast<size_type>(std::distance(first, last))}, _cap{_sz}, _data{alloc_traits::allocate(_alloc, _cap)}
         {
             std::uninitialized_copy(first, last, _data);
         }
 
-        vector(const vector& other) :
+        constexpr vector(const vector& other) :
             _alloc{alloc_traits::select_on_container_copy_construction(other.get_allocator())},
             _sz{other._sz},
             _cap{other._cap},
@@ -65,7 +67,7 @@ namespace mystl
             std::uninitialized_copy_n(other._data, other._sz, _data);
         }
 
-        vector(vector&& other) :
+        constexpr vector(vector&& other) noexcept :
             _alloc{std::move(other._alloc)},
             _sz{std::move(other._sz)},
             _cap{std::move(other._cap)},
@@ -80,7 +82,7 @@ namespace mystl
         {}
         /**********************************************************************/
 
-        ~vector()
+        constexpr ~vector()
         {
             if (_data == nullptr)
                 return;
@@ -92,7 +94,7 @@ namespace mystl
 
         // Assignments
         /**********************************************************************/
-        vector& operator=(const vector& other)
+        constexpr vector& operator=(const vector& other)
         {
             if (this == &other)
                 return *this;
@@ -102,7 +104,7 @@ namespace mystl
             return * this;
         }
 
-        vector& operator=(vector&& other)
+        constexpr vector& operator=(vector&& other)
         {
             if (this == &other)
                 return *this;
@@ -115,25 +117,70 @@ namespace mystl
         }
         /**********************************************************************/
 
-        reference operator[](size_type pos) { return _data[pos]; }
-        const_reference operator[](size_type pos) const { return _data[pos]; }
+        // Element access
+        /**********************************************************************/
+        constexpr reference operator[](size_type pos) { return _data[pos]; }
+        constexpr const_reference operator[](size_type pos) const { return _data[pos]; }
 
-        void push_back(const T& value)
+        constexpr reference at(size_type pos)
+        {
+            check_bounds(pos);
+            return (*this)[pos];
+        }
+        constexpr const_reference at(size_type pos) const
+        {
+            check_bounds(pos);
+            return (*this)[pos];
+        }
+
+        constexpr reference front() { return _data[0]; }
+        constexpr const_reference front() const { return _data[0]; }
+        constexpr reference back() { return _data[_sz - 1]; }
+        constexpr const_reference back() const { return _data[_sz - 1]; }
+        constexpr pointer data() noexcept { return _data; }
+        constexpr const_pointer data() const noexcept{ return _data; }
+        /**********************************************************************/
+
+        // Iterators
+        /**********************************************************************/
+        constexpr iterator begin() noexcept { return _data; }
+        constexpr const_iterator begin() const noexcept { return _data; }
+        constexpr const_iterator cbegin() const noexcept { return _data; }
+        constexpr iterator end() noexcept { return _data + _sz; }
+        constexpr const_iterator end() const noexcept { return _data + _sz; }
+        constexpr const_iterator cend() const noexcept { return _data + _sz; }
+        constexpr reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+        constexpr const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+        constexpr const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(cend()); }
+        constexpr reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+        constexpr const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+        constexpr const_reverse_iterator crend() const noexcept { return const_reverse_iterator(cbegin()); }
+        /**********************************************************************/
+
+        // Modifiers
+        /**********************************************************************/
+        constexpr void push_back(const T& value)
         {
             maybe_expand();
             alloc_traits::construct(_alloc, _data + _sz++, value);
         }
-        void push_back(T&& value)
+        constexpr void push_back(T&& value)
         {
             maybe_expand();
             alloc_traits::construct(_alloc, _data + _sz++, std::move(value));
         }
+        /**********************************************************************/
 
-        bool empty() const { return _sz == 0; }
-        size_type size() const { return _sz; }
-        allocator_type get_allocator() const { return _alloc; }
+        // Capacity
+        /**********************************************************************/
+        constexpr bool empty() const noexcept { return _sz == 0; }
+        constexpr size_type size() const noexcept { return _sz; }
+        constexpr size_type capacity() const noexcept { return _cap; }
+        /**********************************************************************/
 
-        void swap(vector& other)
+        constexpr allocator_type get_allocator() const noexcept { return _alloc; }
+
+        constexpr void swap(vector& other) noexcept
         {
             if constexpr (alloc_traits::propagate_on_container_swap::value)
                 std::swap(_alloc, other._alloc);
@@ -148,22 +195,27 @@ namespace mystl
         size_type _sz, _cap;
         pointer _data;
 
-        void maybe_expand()
+        constexpr void maybe_expand()
         {
             if (_sz < _cap)
                 return;
 
             auto newCap = _cap == 0 ? 1 : _cap;
-            while (newCap < _sz) newCap <<= 1; // Keep doubling until large enough
+            while (newCap <= _sz) newCap <<= 1; // Keep doubling until large enough
             auto newData = alloc_traits::allocate(_alloc, newCap);
-            std::uninitialized_move_n(_data, _sz, newData);
+            std::uninitialized_copy_n(_data, _sz, newData);
 
             std::swap(_cap, newCap);
             std::swap(_data, newData);
-            // Shouldn't need to destroy old array elements
             for (size_type i = 0; i < _sz; ++i)
                 alloc_traits::destroy(_alloc, newData + i);
             alloc_traits::deallocate(_alloc, newData, newCap);
+        }
+
+        inline constexpr void check_bounds(size_type pos) const
+        {
+            if (pos >= _sz)
+                throw std::out_of_range{std::format("vector: index {} out of range of size {}", pos, _sz)};
         }
     };
 }
